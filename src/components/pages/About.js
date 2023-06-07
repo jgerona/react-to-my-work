033fdd0a2afc117510d214fee2cfc715910aea16
Joseph <jwillgerona@gmail.com>
2023-06-07 13:22:57 -0700

    style changes to about.js contact.js and additions to portfolio.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
+import myPic from "../../assets/mypic.png";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Me Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="mt-2">
+      <h1 className="ms-5">Joseph Gerona</h1>
+      <div className="container justify-content-center align-items-center mt-2">
+        <div className="d-flex justify-content-center flex-row center">
+          <img src={myPic} width={"66%"} className="center" alt="my-pic"></img>
+        </div>
+        <div className="mt-2 d-flex flex-row center justify-content-center">
+          <h3 className="text-align-center">
+            Hello! I'm a Full Stack Developer living in Seattle. I recieved my
+            B.S. in Mathematics from the University of Washington, Bothell. I
+            then pursued my passion of computer science through web development
+            and building applications!
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,56 +1,73 @@
-import React from 'react';
-import { useState } from 'react';
-import { validateEmail } from '../../util/helper';
+import React from "react";
+import { useState } from "react";
+import { validateEmail } from "../../util/helper";
 
 export default function Contact() {
-  const [username, setUserame] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [username, setUserame] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(!(validateEmail(e.target.email.value))){
-      alert("Please include a username and a valid email.")
+    if (!validateEmail(e.target.email.value)) {
+      alert("Please include a username and a valid email.");
     } else {
-      setUserame('');
-      setEmail('');
-      setMessage('');
-      alert("Thank you!")
+      setUserame("");
+      setEmail("");
+      setMessage("");
+      alert("Thank you!");
     }
     // Send email/message using backend or email service of choice
     // Clear input fields
   };
 
   return (
-    <form onSubmit={handleSubmit} className='justify-content-center'>
-      <div>
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        value={username}
-        onChange={(e) => setUserame(e.target.value)}
-      />
+    <div className="container mt-4">
+      <div className="align-items-center justify-content-center">
+        <form
+          onSubmit={handleSubmit}
+          className="row g-2"
+        >
+          <div className="mb-3 row d-inline-flex">
+            <label htmlFor="username" className="form-control">
+              Your name:
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUserame(e.target.value)}
+            />
+          </div>
+          <div className="mb-3 row d-inline-flex">
+            <label htmlFor="email" className="form-control">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3 row d-inline-flex">
+            <label htmlFor="message" className="form-control">
+              Message:
+            </label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      </div>
-      <div>
-      <label htmlFor="message">Message:</label>
-      <textarea
-        id="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      </div>
-      <div><button type="submit">Submit</button></div>
-    </form>
+    </div>
   );
 }
